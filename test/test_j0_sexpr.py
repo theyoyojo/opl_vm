3690758c7701683e4c716b0671b0d16a43b58d5c
Joel Savitz <joelsavitz@gmail.com>
2019-09-08 22:33:25 -0400

    Completed "#6 - rewrite test suite in sexpressions"

--- test/test_j0_sexpr.py
+++ test/test_j0_sexpr.py
@@ -39,18 +39,22 @@ def test_add_mult_number_left_sag():
 #     assert y.number == 33
 
 def test_add_mult_number_right_sag():
-    pass
-#     x = e.Add(v.Number(4), e.Mult(v.Number(7), v.Number(5)))
-    # x = s.Cons(s.Atom("+"),s.Cons(
-    #     s.Atom("*"),s.Cons(s.Atom("4"),s.Atom("7"))),
-    #     s.Cons(s.Atom("5"),s.Nil())))
-#     x.pp()
+    x = s.Cons(s.Atom("+"),s.Cons(s.Atom("4"),
+        s.Cons(s.Atom("*"),s.Cons(s.Atom("7"),
+            s.Cons(s.Atom("5"),s.Nil())))))
+    x.pp()
 #     assert x.left.number == 4 and x.right.left.number == 7 and x.right.right.number == 5
 #     y = x.ibig()
 #     y.pp()
 #     assert y.number == 39
 
-# def test_mult_add_number_left_sag():
+def test_mult_add_number_left_sag():
+    x = s.Cons(s.Atom("*"),s.Cons(s.Cons(
+        s.Atom("+"),s.Cons(s.Atom("4"),
+        s.Cons(s.Atom("7"),s.Nil()))),
+        s.Cons(s.Atom("5"),s.Nil())))
+    x.pp()
+
 #     x = e.Mult(e.Add(v.Number(4), v.Number(7)),v.Number(5))
 #     x.pp()
 #     assert x.left.left.number == 4 and x.left.right.number == 7 and x.right.number == 5
@@ -58,7 +62,11 @@ def test_add_mult_number_right_sag():
 #     y.pp()
 #     assert y.number == 55
 
-# def test_mult_add_number_right_sag():
+def test_mult_add_number_right_sag():
+    x = s.Cons(s.Atom("*"),s.Cons(s.Atom("4"),
+        s.Cons(s.Atom("+"),s.Cons(s.Atom("7"),
+            s.Cons(s.Atom("5"),s.Nil())))))
+    x.pp()
 #     x = e.Mult(v.Number(4), e.Add(v.Number(7), v.Number(5)))
 #     x.pp()
 #     assert x.left.number == 4 and x.right.left.number == 7 and x.right.right.number == 5
@@ -66,40 +74,54 @@ def test_add_mult_number_right_sag():
 #     y.pp()
 #     assert y.number == 48
 
-# def test_add_add_add_balanced():
-#     x = e.Add( \
-#             e.Add(v.Number(4), v.Number(7)), \
-#             e.Add(v.Number(5), v.Number(2)))
-#     x.pp()
-#     assert x.left.left.number == 4 and x.left.right.number == 7 \
-#         and x.right.left.number == 5 and x.right.right.number == 2
+def test_add_add_add_balanced():
+    x = s.Cons(s.Atom("+"),s.Cons(s.Cons(
+        s.Atom("+"),s.Cons(s.Atom("4"),
+        s.Cons(s.Atom("7"),s.Nil()))),
+        s.Cons(s.Atom("+"),s.Cons(s.Atom("5"),
+            s.Cons(s.Atom("2"),s.Nil())))))
+    x.pp()
 #     y = x.ibig()
 #     y.pp()
 #     assert y.number == 18
 
-# def test_mult_mult_mult_balanced():
+def test_mult_mult_mult_balanced():
+    x = s.Cons(s.Atom("*"),s.Cons(s.Cons(
+        s.Atom("*"),s.Cons(s.Atom("4"),
+        s.Cons(s.Atom("7"),s.Nil()))),
+        s.Cons(s.Atom("*"),s.Cons(s.Atom("5"),
+            s.Cons(s.Atom("2"),s.Nil())))))
 #     x = e.Mult( \
 #             e.Mult(v.Number(4), v.Number(7)), \
 #             e.Mult(v.Number(5), v.Number(2)))
-#     x.pp()
+    x.pp()
 #     assert x.left.left.number == 4 and x.left.right.number == 7 \
 #         and x.right.left.number == 5 and x.right.right.number == 2
 #     y = x.ibig()
 #     y.pp()
 #     assert y.number == 280
 
-# def test_add_mult_add_left_sag():
+def test_add_mult_add_left_sag():
 #     x = e.Add(e.Mult(e.Add( \
 #             v.Number(4), v.Number(7)), \
 #             v.Number(5)), v.Number(2))
-#     x.pp()
+    x = s.Cons(s.Atom("+"),s.Cons(s.Cons(s.Atom("*"),s.Cons(
+        s.Cons(s.Atom("+"),s.Cons(s.Atom("4"),s.Cons(s.Atom("7"),s.Nil()))),
+        s.Cons(s.Atom("5"),s.Nil()))),s.Cons(s.Atom("2"),s.Nil())))
+
+    x.pp()
 #     assert x.left.left.left.number == 4 and x.left.left.right.number == 7 \
 #         and x.left.right.number == 5 and x.right.number == 2
 #     y = x.ibig()
 #     y.pp()
 #     assert y.number == 57
 
-# def test_mult_add_mult_left_sag():
+def test_mult_add_mult_left_sag():
+    x = s.Cons(s.Atom("*"),s.Cons(s.Cons(s.Atom("+"),s.Cons(
+        s.Cons(s.Atom("*"),s.Cons(s.Atom("4"),s.Cons(s.Atom("7"),s.Nil()))),
+        s.Cons(s.Atom("5"),s.Nil()))),s.Cons(s.Atom("2"),s.Nil())))
+
+    x.pp()
 #     x = e.Mult(e.Add(e.Mult( \
 #             v.Number(4), v.Number(7)), \
 #             v.Number(5)), v.Number(2))
@@ -110,13 +132,17 @@ def test_add_mult_number_right_sag():
 #     y.pp()
 #     assert y.number == 66
 
-# def test_add_add_add_add_left_sag():
+def test_add_add_add_add_left_sag():
 #     x = e.Add(e.Add(e.Add(e.Add( \
 #         v.Number(4),v.Number(7)), \
 #         v.Number(5)),v.Number(2)), \
 #         v.Number(6))
+    x = s.Cons(s.Atom("+"),s.Cons(s.Cons(s.Atom("+"),s.Cons(s.Cons(s.Atom("+"),s.Cons(
+            s.Cons(s.Atom("+"),s.Cons(s.Atom("4"),s.Cons(s.Atom("7"),s.Nil()))),
+            s.Cons(s.Atom("5"),s.Nil()))),s.Cons(s.Atom("2"),s.Nil()))),
+            s.Cons(s.Atom("6"),s.Nil())))
 
-#     x.pp()
+    x.pp()
 #     assert x.left.left.left.left.number == 4 and x.left.left.left.right.number == 7 \
 #         and x.left.left.right.number == 5 and x.left.right.number == 2 and x.right.number == 6
 #     y = x.ibig()
