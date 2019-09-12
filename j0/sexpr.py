c76f64bf2c3eb3745e6ef2800c34b87bef342eb5
Joel Savitz <joelsavitz@gmail.com>
2019-09-11 20:36:50 -0400

    rewrite desugar but it still has problems

--- j0/sexpr.py
+++ j0/sexpr.py
@@ -16,6 +16,7 @@ class Sexpr:
             return 1 + self.right.length()
         else:
             raise SaltySyntax("expected a Cons or Nil sexpr, not this garbage")
+
     def desu(self):
         return desugar(self)
 
@@ -23,7 +24,24 @@ class Sexpr:
         if isinstance(self, Nil):
             return Nil()
         elif isinstance(self, Cons):
+            return self.left
+        else:
+            raise SaltySyntax("expected a Cons or Nil sexpr, not this garbage")
+
+    def rest(self):
+        if isinstance(self, Nil):
+            return Nil()
+        elif isinstance(self, Cons):
             return self.right
+        else:
+            raise SaltySyntax("expected a Cons or Nil sexpr, not this garbage")
+        
+
+    def second(self):
+        if isinstance(self, Nil):
+            return Nil()
+        elif isinstance(self, Cons):
+            return self.right.left
         else:
             raise SaltySyntax("expected a Cons or Nil sexpr, not this garbage")
 
@@ -41,9 +59,6 @@ class Nil(Sexpr):
     def pp(self):
         print("\n" + self.repr() + "\n")
 
-    def desu(self):
-        return
-
 class Atom(Sexpr):
     def __init__(self, value):
         self.value = value
@@ -53,12 +68,6 @@ class Atom(Sexpr):
 
     def pp(self):
         print("\n" + self.repr() + "\n")
-
-    # def desu(self):
-    #     if is_number(self.value):
-    #         return v.Number(float(self.value))
-    #     else:
-    #         raise SaltySyntax("Not sure how to desugar this atom")
 
 class Cons(Sexpr):
     def __init__(self, left, right):
@@ -96,8 +105,71 @@ class Cons(Sexpr):
     #         raise SaltySyntax("Something went wrong :)")
 
 
+def desugar_minus(sexpr):
+    # Unary case
+    if sexpr.length() == 1:
+        return e.Mult(v.Number(-1), sexpr.first().desu())
+    # Binary case
+    elif sexpr.length() == 2:
+        return e.Add(sexpr.first().desu(),
+            e.Mult(v.Number(-1), sexpr.second().desu()))
+    else:
+        raise SaltySyntax("- function expects 1 or two arguments, got " + 
+                sexpr.length() + " instead")
+
+def desugar_plus(sexpr):
+    # Base case
+    if sexpr.length() == 1:
+        return sexpr.first().desu()
+    # Recursive case
+    elif sexpr.length() > 1:
+        return e.Add(sexpr.first().desu(), sexpr.rest().desu())
+    else:
+        raise SaltySyntax("unknown error. Negative length?")
+    
+    
+
+def desugar_mult(sexpr):
+    # Base case
+    if sexpr.length() == 1:
+        return sexpr.first().desu()
+    # Recursive case
+    elif sexpr.length() > 1:
+        return e.Mult(sexpr.first().desu(), sexpr.rest().desu())
+    # Error case (basement case)
+    else:
+        raise SaltySyntax("* function expects more than 0 arguments")
+
+antirecipies = { \
+        "-": desugar_minus,
+        "+": desugar_plus,
+        "*": desugar_mult
+        }
+
+def desugar_cons(sexpr):
+    action = sexpr.first().repr()
+    if action in antirecipies:
+        return antirecipies[action](sexpr.rest())
+    elif sexpr.length() == 1:
+        return sexpr.first().desu()
+    else:
+        raise SaltySyntax("Unknown function \"" + action + "\"")
+
+def desugar_atom(sexpr):
+    if is_number(sexpr.repr()):
+        return v.Number(float(sexpr.repr()))
+    else:
+        raise SaltySyntax("Unsure how to desugar atomic sexpr \"" + sexpr.repr() + "\"")
 
 def desugar(sexpr):
-    print(sexpr.length())
-
-
+    #print("sexpr length: " + str(sexpr.length())
+    sexpr.pp()
+    if isinstance(sexpr, Cons):
+        return desugar_cons(sexpr)
+    elif isinstance(sexpr, Atom):
+        return desugar_atom(sexpr)
+    elif isinstance(sexpr, Nil):
+        raise SaltySyntax("Cannot desugar a null cons cell")
+    else:
+        raise SaltySyntax("Unknown object, giving up")
+    
